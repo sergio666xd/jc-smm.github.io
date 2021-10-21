4a4fc232e8224603347522b9489167de68fc6c25
sergio666xd <sergiomunoz06062006@gmail.com>
2021-10-20 22:52:48 -0500

    Update main.js

--- docs/activities/a/Calculadora/main.js
+++ docs/activities/a/Calculadora/main.js
@@ -2,27 +2,27 @@ function sumar(){
     var a = parseInt(document.getElementById("a").value);
     var b = parseInt(document.getElementById("b").value);
     document.getElementById('result')
-    .innerHTML = "Resultado: " + (a+b);
-    console.log ("Resultado de la Suma: " + (a+b));
+        .innerHTML = "Resultado: " + a + "+" + b + " = " + (a+b);
+    console.log("Resultado de la Suma (" + a + "+" + b +"): " + (a+b));
 }
 function restar(){
     var a = parseInt(document.getElementById("a").value);
     var b = parseInt(document.getElementById("b").value);
     document.getElementById('result')
-    .innerHTML = "Resultado: " + (a-b);
-    console.log ("Resultado de la Resta: " + (a-b));
+    .innerHTML = "Resultado: " + a + "-" + b + " = " + (a-b);
+    console.log("Resultado de la Resta (" + a + "-" + b +"): " + (a-b));
 }
 function multiplicar(){
     var a = parseInt(document.getElementById("a").value);
     var b = parseInt(document.getElementById("b").value);
     document.getElementById('result')
-    .innerHTML = "Resultado: " + (a*b);
-    console.log ("Resultado de la Multiplicación: " + (a*b));
+    .innerHTML = "Resultado: " + a + "*" + b + " = " + (a*b);
+    console.log("Resultado de la Multiplicación (" + a + "*" + b +"): " + (a*b));
 }
 function dividir(){
     var a = parseInt(document.getElementById("a").value);
     var b = parseInt(document.getElementById("b").value);
     document.getElementById('result')
-    .innerHTML = "Resultado: " + (a/b);
-    console.log ("Resultado de la División: " + (a/b));
+    .innerHTML = "Resultado: " + a + "÷" + b + " = " + (a/b);
+    console.log("Resultado de la División (" + a + "÷" + b +"): " + (a/b));
 }
