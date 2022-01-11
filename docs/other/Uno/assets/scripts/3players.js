65ee5efd13821f98a1bc6ff9d23d3e04ad8a1578
sergio666xd <sergiomunoz06062006@gmail.com>
2022-01-11 13:17:05 -0500

    Modo 3 jugadores trerminado

--- docs/other/Uno/assets/scripts/3players.js
+++ docs/other/Uno/assets/scripts/3players.js
@@ -1,32 +1,53 @@
-posi = 1;
 namesP3 = ["Aleja", "Sergio", "Eunice"];
-pCant = namesP3.length-1;
+pCant3 = namesP3.length-1;
 
-$("div").delegate("button", "click", function () {
-    button = $(this).attr("id");
-    buttonh = $(this).attr("id")+": "+$(this).attr("value");
-    document.querySelector("#alertDiv").innerHTML += buttonh + "<hr>";
+function threeP(button,ctrl) {
     if (button == "three") {
         setTimeout(() => {
             const textname = document.querySelector("#name");
             const right = document.querySelector("#right");
             const left = document.querySelector("#left");
             textname.innerHTML = namesP3[posi];
+            nameNext();
+            document.querySelector(".control-btns").innerHTML = ctrl;
+            gameThree();
+			goBack();
         }, 1000);
     }
-});
+}
+
+function nameNext() {
+    if ($(".grand__img img").hasClass("rotate")) {
+        if (posi == 0) {
+            posiN = pCant3;
+        } else if (posi == 1) {
+            posiN = 0;
+        } else {
+            posiN = posi - 1;
+        }
+    } else {
+        if (posi == pCant3) {
+            posiN = 0;
+        } else if (posi == (pCant3-1)) {
+            posiN = pCant3;
+        } else {
+            posiN = posi + 1;
+        }
+    }
+    document.querySelector("#nextName").innerHTML = namesP3[posiN];
+}
 
 function negative() {
-    if (posi == pCant) {
+    if (posi == pCant3) {
         posi = posi - 1;
     } else if (posi == 0) {
-        posi = pCant;
+        posi = pCant3;
     } else {
         posi = posi - 1;
     }
 }
 function positive() {
-    if (posi == pCant) {
+    if (posi == pCant3) {
         posi = 0;
     } else if (posi == 0) {
         posi = posi + 1;
@@ -38,10 +59,12 @@ function positive() {
 function changeName() {
     setTimeout(() => {
         document.querySelector("#name").innerHTML = namesP3[posi];
+        nameNext()
     }, 500);
 }
 function nextName() {
     document.querySelector("#name").innerHTML = namesP3[posi];
+    nameNext()
 }
 
 function arrowright() {
@@ -68,73 +91,75 @@ function arrowleft() {
 //     changeName();
 // })
 
-$("#change").click(function () {
-    let namesP3 = ["Aleja", "Sergio", "Eunice"];
-    $(".grand__img img").toggleClass("rotate");
-    // rotate.classList.toggle("rotate");
-    setTimeout(() => {
+function gameThree() {
+    $("#change").click(function () {
+        let namesP3 = ["Aleja", "Sergio", "Eunice"];
+        $(".grand__img img").toggleClass("rotate");
+        // rotate.classList.toggle("rotate");
+        setTimeout(() => {
+            if ($(".grand__img img").hasClass("rotate")) {
+                arrowleft();
+                negative();
+            } else {
+                arrowright();
+                positive();
+            }
+        }, 500)
+        changeName();
+    });
+    
+    // next.addEventListener("click", function () {
+    //     if (rotate.classList.contains("rotate")) {
+    //         if (posi == 0) {
+    //             posi = pCant3;
+    //         } else {
+    //             posi = posi - 1;
+    //         }
+    //     } else {
+    //         if (posi == pCant3) {
+    //             posi = 0;
+    //         } else {
+    //             posi = posi + 1;
+    //         }
+    //     }
+    //     nextName();
+    // })
+    
+    $("#next").click(function () {
         if ($(".grand__img img").hasClass("rotate")) {
-            arrowleft();
-            negative();
+            if (posi == 0) {
+                posi = pCant3;
+            } else {
+                posi = posi - 1;
+            }
         } else {
-            arrowright();
-            positive();
+            if (posi == pCant3) {
+                posi = 0;
+            } else {
+                posi = posi + 1;
+            }
         }
-    }, 500)
-    changeName();
-});
-
-// next.addEventListener("click", function () {
-//     if (rotate.classList.contains("rotate")) {
-//         if (posi == 0) {
-//             posi = pCant;
-//         } else {
-//             posi = posi - 1;
-//         }
-//     } else {
-//         if (posi == pCant) {
-//             posi = 0;
-//         } else {
-//             posi = posi + 1;
-//         }
-//     }
-//     nextName();
-// })
-
-$("#next").click(function () {
-    if ($(".grand__img img").hasClass("rotate")) {
-        if (posi == 0) {
-            posi = pCant;
+        nextName();
+    }); 
+    
+    $("#ban").click(function () {
+        if ($(".grand__img img").hasClass("rotate")) {
+            if (posi == 0) {
+                posi = pCant3-1;
+            } else if (posi == 1) {
+                posi = pCant3;
+            } else {
+                posi = posi - 2;
+            }
         } else {
-            posi = posi - 1;
+            if (posi == pCant3) {
+                posi = 1;
+            } else if (posi == (pCant3-1)) {
+                posi = 0;
+            } else {
+                posi = posi + 2;
+            }
         }
-    } else {
-        if (posi == pCant) {
-            posi = 0;
-        } else {
-            posi = posi + 1;
-        }
-    }
-    nextName();
-}); 
-
-$("#ban").click(function () {
-    if ($(".grand__img img").hasClass("rotate")) {
-        if (posi == 0) {
-            posi = pCant-1;
-        } else if (posi == 1) {
-            posi = pCant;
-        } else {
-            posi = posi - 2;
-        }
-    } else {
-        if (posi == pCant) {
-            posi = 1;
-        } else if (posi == (pCant-1)) {
-            posi = 0;
-        } else {
-            posi = posi + 2;
-        }
-    }
-    nextName();
-});
+        nextName();
+    });
+}
--- docs/other/Uno/assets/scripts/main.js
+++ docs/other/Uno/assets/scripts/main.js
@@ -1,11 +1,16 @@
+posi = 1;
+controlBtns = '<button class="btn btn-primary mt-3" id="change"><i class="fas fa-exchange-alt"></i> Cambiar sentido</button><button class="btn btn-success mt-3" id="next"><span id="left"></span> <span id="text"> Siguiente </span> <span id="right"><i class="fas fa-arrow-right"></i></span></button><button class="btn btn-danger mt-3" id="ban"><i class="fas fa-ban"></i> Bloqueo</button>';
+
 $('#game').load('./assets/game/1select.html');
 $(document).ready(() => {
 	$("div").delegate("button", "click", function () {
 		button = $(this).attr("id");
+		if (button=="two"||button=="three") {
+			$('#game').load('./assets/game/play.html');
+		}
 		if (button == "two") {
-			$('#game').load('./assets/game/2players.html');
 		} else if (button == "three") {
-			$('#game').load('./assets/game/3players.html');
+			threeP(button,controlBtns);
 		}
 	});
 	// two.addEventListener("click", function (){
@@ -15,6 +20,11 @@ $(document).ready(() => {
 	// });
 });
 
+function goBack() {
+	$("#back").click(function () {
+		$("#game").load("./assets/game/1select.html");
+	})
+}
 
 function color() {
     if (nombreColor == "red") {
